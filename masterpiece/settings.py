6a33c187f5d088d3738a4dcc804f9677748a18c2
Joonas Timlin <joonas@timlins.se>
2023-10-08 20:00:50 +0000

    fix heroku allowed hosts address

--- masterpiece/settings.py
+++ masterpiece/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jindah-masterpiece-o0qms0hmslf.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['masterpiece23-cbdad7ea4f9e.herokuapp.com', '8000-jindah-masterpiece-o0qms0hmslf.ws-eu105.gitpod.io']
 
 
 # Application definition
